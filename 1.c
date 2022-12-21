e6105f9a66859be27b868e2fa1a1ee98188d2013
lokeshyc01 <100062082+lokeshyc01@users.noreply.github.com>
2022-12-21 15:28:31 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,32 +1,34 @@
 #include<stdio.h>
 
-void Pattern(int iRow,int iCol)
+void Pattern(int iRow , int iCol)
 {
-    int i = 0, j = 0,iCnt = 1;
+    int i = 0, j = 0;
 
     for(i = 1 ; i <= iRow ; i++)
     {
-        for(j = 1 ; j <= iCol ; j++)
+        for(j = 1; j <= iCol ; j++)
         {
-            if(iCnt > 9)
+            if(i >= j)
             {
-                iCnt = 1;
+                printf("*\t");
             }
-            printf("%d\t",iCnt);
-            iCnt++;
+            else
+            {
+                printf("#\t");
+            }
         }
         printf("\n");
     }
 }
-
 int main()
 {
-    int iValue1 = 0,iValue2 = 0;
+    int iValue1 = 0, iValue2 = 0;
 
-    printf("Enter Number of Rows and Columns\n");
+    printf("Enter the number of rows and columns \n");
     scanf("%d %d",&iValue1,&iValue2);
 
     Pattern(iValue1,iValue2);
 
     return 0;
+
 }
--- 5.c
+++ 5.c
@@ -1,26 +1,34 @@
- #include<stdio.h>
+#include<stdio.h>
 
-void Pattern(int iRow,int iCol)
+void Pattern(int iRow , int iCol)
 {
-    int i = 0, j = 0,iCnt = 1;
-    for(i = 1; i< iRow; i++)
+    int i = 0, j = 0;
+
+    for(i = 1 ; i <= iRow ; i++)
     {
-        for(j = 1 ; j <= iRow ; j++)
+        for(j = 1; j <= iCol ; j++)
         {
-            printf("%d\t",iCnt++);
+            if(i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf("*\t");
+            }
         }
         printf("\n");
     }
 }
-
 int main()
 {
     int iValue1 = 0, iValue2 = 0;
 
-    printf("Enter number of rows and Columns \n");
+    printf("Enter the number of rows and columns \n");
     scanf("%d %d",&iValue1,&iValue2);
 
     Pattern(iValue1,iValue2);
 
     return 0;
+
 }
